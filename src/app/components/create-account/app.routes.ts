716987296693ee4d0eb7f6beeb009d684bb649cc
Lu1sAndr3sR <159199988+Lu1sAndr3sR@users.noreply.github.com>
2024-05-11 23:51:34 -0600

    update-front3

---
+++ src/app/components/create-account/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
+
+export const routes: Routes = [
+
+    {path: 'log-in', component: LogInComponent},
+];
---
+++ src/app/components/salary-type/salary-type.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-type',
+  standalone: true,
+  imports: [],
+  templateUrl: './salary-type.component.html',
+  styleUrl: './salary-type.component.css'
+})
+export class SalaryTypeComponent {
+
+}
