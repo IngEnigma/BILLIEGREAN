30dc2fef182babf8f3b7de92925bf65dc7b25e30
Lu1sAndr3sR <159199988+Lu1sAndr3sR@users.noreply.github.com>
2024-05-19 02:15:37 -0600

    update-front5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { SalaryTypeComponent } from './components/salary-type/salary-type.component';
+import { HomeComponent } from './components/home/home.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { NewPasswordComponent } from './components/new-password/new-password.component';
+import { ListaGastosComponent } from './components/lista-gastos/lista-gastos.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'log-in', component: LogInComponent},
+    {path: 'create-account', component: CreateAccountComponent},
+    {path: 'salary-type', component: SalaryTypeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'forgot-password', component: ForgotPasswordComponent},
+    {path: 'new-password', component: NewPasswordComponent},
+    {path: 'lista-gastos', component: ListaGastosComponent},
+    {path: 'calculadora', component: CalculadoraComponent},
+];
+
